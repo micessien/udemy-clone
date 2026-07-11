be398f94ce0811f40e61b74b52dcea25bcbf66d7
micessien <micaeldie@gmail.com>
2026-07-11 17:59:24 +0000

    Make mobile sidebar

--- app/(dashboard)/_components/sidebar-item.tsx
+++ app/(dashboard)/_components/sidebar-item.tsx
@@ -28,12 +28,12 @@ const SidebarItem = ({ icon: Icon, label, href }: SidebarItemProps) => {
       onClick={onClick}
       type="button"
       className={cn(
-        "flex items-center gab-x-2 text-slate-500 text-sm font-medium pl-6 transition-all hover:text-slate-600 hover:bg-slate-300/20",
+        "flex items-center gap-x-2 text-slate-500 text-sm font-medium pl-6 transition-all hover:text-slate-600 hover:bg-slate-300/20",
         isActive &&
           "text-sky-700 bg-sky-200/20 hover:bg-sky-200/20 hover:text-sky-700",
       )}
     >
-      <div className="flex items-center gab-x-3 py-4">
+      <div className="flex items-center gap-x-3 py-4">
         <Icon
           size={22}
           className={cn("text-slate-500", isActive && "text-sky-700")}
