0e8d9c3ed5f06c0df8bef5c543f4787ea1477170
micessien <micaeldie@gmail.com>
2026-07-12 16:05:20 +0000

    Reorder chapters list working

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
@@ -15,6 +15,7 @@ import ImageForm from "./_components/image-form";
 import CategoryForm from "./_components/category-form";
 import PriceForm from "./_components/price-form";
 import AttachmentForm from "./_components/attachment-form";
+import ChaptersFormForm from "./_components/chapters-form";
 
 const CourseIdPage = async ({
   params,
@@ -31,8 +32,14 @@ const CourseIdPage = async ({
   const course = await db.course.findUnique({
     where: {
       id: courseId,
+      userId,
     },
     include: {
+      chapters: {
+        orderBy: {
+          position: "asc",
+        },
+      },
       attachments: {
         orderBy: {
           createdAt: "desc",
@@ -56,6 +63,7 @@ const CourseIdPage = async ({
     course.imageUrl,
     course.price,
     course.categoryId,
+    course.chapters.some((chapter) => chapter.isPublished),
   ];
   const totalFields = requiredFields.length;
   const completedFields = requiredFields.filter(Boolean).length;
@@ -95,7 +103,7 @@ const CourseIdPage = async ({
               <IconBadge icon={ListChecks} />
               <h2 className="text-xl">Course chapters</h2>
             </div>
-            <div>TODO: Chapters</div>
+            <ChaptersFormForm initialData={course} courseId={courseId} />
           </div>
           <div>
             <div className="flex items-center gap-x-2">
---
+++ app/api/courses/[courseId]/chapters/reorder/route.ts
@@ -0,0 +1,41 @@
+import { auth } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
+
+import { db } from "@/lib/db";
+
+export async function PUT(
+  req: Request,
+  { params }: { params: Promise<{ courseId: string }> },
+) {
+  try {
+    const { userId } = await auth();
+    const { courseId } = await params;
+    const { list } = await req.json();
+
+    if (!userId) {
+      return new NextResponse("Unauthorized", { status: 401 });
+    }
+
+    const courseOwner = await db.course.findUnique({
+      where: {
+        id: courseId,
+        userId,
+      },
+    });
+    if (!courseOwner) {
+      return new NextResponse("Unauthorized", { status: 401 });
+    }
+
+    for (const item of list) {
+      await db.chapter.update({
+        where: { id: item.id },
+        data: { position: item.position },
+      });
+    }
+
+    return new NextResponse("Success", { status: 200 });
+  } catch (error) {
+    console.log("[REORDER]", error);
+    return new NextResponse("Internal error", { status: 500 });
+  }
+}
