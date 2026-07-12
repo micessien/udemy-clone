743da70fd9e05c18c517092f4a78fefb86c1f9d9
micessien <micaeldie@gmail.com>
2026-07-12 16:37:36 +0000

    Edit chapter title

---
+++ app/api/courses/[courseId]/chapters/[chapterId]/route.ts
@@ -0,0 +1,45 @@
+import { auth } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
+
+import { db } from "@/lib/db";
+
+export async function PATCH(
+  req: Request,
+  { params }: { params: Promise<{ courseId: string; chapterId: string }> },
+) {
+  try {
+    const { userId } = await auth();
+    const { courseId, chapterId } = await params;
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const { isPublished, ...values } = await req.json();
+
+    if (!userId) {
+      return new NextResponse("Unauthorized", { status: 401 });
+    }
+
+    const courseOwner = await db.course.findUnique({
+      where: {
+        id: courseId,
+        userId,
+      },
+    });
+    if (!courseOwner) {
+      return new NextResponse("Unauthorized", { status: 401 });
+    }
+
+    const chapter = await db.chapter.update({
+      where: {
+        id: chapterId,
+        courseId,
+      },
+      data: { ...values },
+    });
+
+    // TODO: Handle Video Upload
+
+    return NextResponse.json(chapter);
+  } catch (error) {
+    console.log("[COURSES_CHAPTER_ID]", error);
+    return new NextResponse("Internal error", { status: 500 });
+  }
+}
